5191c01093fb5c0c2360f3cc9dfb3ce31d28f291
dav1jim <davidricardojimenezlopez@gmail.com>
2022-08-29 13:35:04 -0600

    primer commit

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,14 @@
+/*Licencias de uso*/
+
+package com.mycompany.holamundo;
+
+/*Autor@david*/
+
+public class Holamundo {
+   public static void main (String[]args){
+   System.out.println("hola mundo"); //operaciones//    
+   }          
+          
+//Una funcion, en java llamada clase esta compuesto por el nombre de la funcion y luego entre parentesis los tipos de datos que contendra la logica de progarmacion dentro de la funcion// 
+    
+}
